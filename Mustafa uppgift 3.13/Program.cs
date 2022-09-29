9548b75bbda382e07da88d4fa136b4d489e42535
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-29 13:26:06 +0200

    Add project files.

---
+++ Mustafa uppgift 3.13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Uppgift_3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur gammal är du?");
+            double ålder = double.Parse(Console.ReadLine());
+            if (ålder >= 19)
+{
+                Console.WriteLine("du får delta i tävlingen");
+            }
+             else
+            {
+                Console.WriteLine("du får inte delta i tävlingen");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
